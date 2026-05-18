fb942d8046db184dfa6dde9751dc7e0737af1f92
David Stockley <magicspon@gmail.com>
2026-05-18 22:45:59 +0100

    ci: add ci/cd and seed script

--- eslint.config.js
+++ eslint.config.js
@@ -29,6 +29,7 @@ export default [
     plugins: { 'jsx-a11y': jsxA11y },
     rules: {
       'no-restricted-exports': 'off',
+      'perfectionist/sort-objects': 'off',
       'jsx-a11y/control-has-associated-label': [
         'warn',
         {
